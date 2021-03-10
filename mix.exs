db430182a157e2ce2a4d9877fbe2233458ed3a27
Daniel José Domingos Serrano <danieljdserrano@gmail.com>
2021-03-10 22:14:49 +0000

    Bump 0.2.2 ➜ 0.2.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Firefighter.MixProject do
   def project do
     [
       app: :firefighter,
-      version: "0.2.2",
+      version: "0.2.3",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       description: description(),
