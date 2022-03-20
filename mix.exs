66465f1161c70d80df4a76630292d0f0d0583d1a
Daniel Serrano <daniel.serrano@onfido.com>
2022-03-20 18:39:59 +0000

    Version release 0.3.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Firefighter.MixProject do
   def project do
     [
       app: :firefighter,
-      version: "0.2.6",
+      version: "0.3.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       description: description(),
